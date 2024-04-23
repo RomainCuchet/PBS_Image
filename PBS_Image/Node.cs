eb5ea4822efab431d2d94c25927b298e0a05cdc0
alex-crr <alexandre@courri.eu>
2024-04-23 23:14:20 +0200

    Huffmanv1.0
    J'suis blaireau j'ai pas changé de branche
    création des noeuds et de l'arbre d'huffman
    travail préliminaire sur les méthodes:
    -s'accorder avec @thomas pour les sorties attendues
    -recheck bien les tests (les écrire)

--- PBS_Image/Node.cs
+++ PBS_Image/Node.cs
@@ -6,20 +6,36 @@ namespace PBS_Image
     {
         public Node Left { get; set; }
         public Node Right { get; set; }
-        public Pixel Value { get; set; }
+        public Pixel Pixel { get; set; }
         public int Frequency { get; set; }
 
-        public Node(Pixel value, int frequency)
+        /// <summary>
+        /// Crée un noeud à partir d'un pixel et d'une fréquence, éventuellement avec des enfants
+        /// </summary>
+        /// <param name="pixel"></param>
+        /// <param name="frequency"></param>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        public Node(Pixel pixel, int frequency, Node left = null, Node right = null)
         {
-            Value = value;
+            Pixel = pixel;
             Frequency = frequency;
+            Left = left;
+            Right = right;
         }
 
-        public Node(Node left, Node right)
+        /// <summary>
+        /// Crée un noeud à partir de deux enfants et d'un pixel éventuellement 
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <param name="pixel"></param>
+        public Node(Node left, Node right, Pixel pixel = null)
         {
             Left = left;
             Right = right;
             Frequency = left.Frequency + right.Frequency;
+            Pixel = pixel;
         }
 
         public bool IsLeaf()
